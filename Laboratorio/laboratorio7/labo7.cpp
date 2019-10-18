8a9b309c71a6a5c0926be12cb105bd7129202599
MilimetrosDeMercurio <00225117@uca.edu.sv>
2019-10-18 17:24:35 -0600

    RodCutting

---
+++ Laboratorio/laboratorio7/labo7.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+using namespace std;
+
+int p[]={0,1,5,8,9};
+int r[]={-1,-1,-1,-1,-1};
+
+int rodeCutting(int n){
+    r[0]=0;
+    for (int j = 1; j <=n; j++)
+    {
+        int q=-1;
+        for (int i = 1; i <=j; i++)
+        {
+            q = max(q,p[i]+r[j-i]);
+        }
+        r[j]=q;
+        
+    }
+    return r[n];
+    
+}
+
+int main(){
+    cout<<rodeCutting(4)<<endl;
+}
